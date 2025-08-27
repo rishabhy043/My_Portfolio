0db4fa6122399d543c33045eed5cef4895dcfe71
rishabhy043 <rishabh.knipss043@gmail.com>
2025-08-27 15:49:00 +0530

    App.jsx done

--- rishabhportfolio/src/App.jsx
+++ rishabhportfolio/src/App.jsx
@@ -1,11 +1,25 @@
+
 import './App.css'
+import Contact from './components/contact/contact'
+import Skills from './components/Dosection/do'
+import Education from './components/Education/Education'
+import Footer from './components/Footer/Footer'
+import Intro from './components/Intro/intro'
+import Works from './components/Myportfoliio/portfolio'
+import Navbar from './components/Navbar/navbar'
 
 function App() {
 
   return (
-   <>
-   
-   </>
+  <div>
+      <Navbar></Navbar>
+      <Intro></Intro>
+      <Skills></Skills>
+      <Works></Works>
+      <Education></Education>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
   )
 }
 
